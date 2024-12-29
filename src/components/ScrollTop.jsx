c9472e043df873918ff874b1b4256528322f79ec
Asadbek <asadbeknan5566@gmail.com>
2024-12-29 11:46:53 +0500

    ref: navbar

---
+++ src/components/ScrollTop.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]); // Runs whenever the pathname changes
+
+  return null;
+}
+
+export default ScrollToTop;
